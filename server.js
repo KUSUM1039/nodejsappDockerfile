3d0223690413570743fb581f7674fec480ea074e
KUSUM1039 <114174483+KUSUM1039@users.noreply.github.com>
2022-11-09 13:55:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
       </head>
       <body>
         <section>
-          <h2>Hello DevOps!!</h2>
+          <h2>Hello DevOpss!!</h2>
           <h3>${userGoal}</h3>
         </section>
         <form action="/store-goal" method="POST">
